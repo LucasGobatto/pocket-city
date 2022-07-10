df0f4797a8c2146b4ce7f0d6214afeb97e0f660e
LucasGobatto <lucas.bisaio@live.com>
2022-07-10 16:41:05 -0300

    feat: add more icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,23 @@
-import factory from "./factory-3-icon.png";
-import house from "./house-2-icon.png";
-import shop from "./shop-1-icon.png";
+import factory from "./factory-icon.png";
+import house from "./house-icon.png";
+import shop from "./shop-icon.png";
+import church from "./church-icon.png";
+import publicTransport from "./public.transport-icon.png";
+import usine from "./usine-icon.png";
+import mall from "./mall-icon.png";
+import cityHall from "./city-hall-icon.png";
+import school from "./school-icon.png";
+import airport from "./airport-icon.png";
 
 export const BuildingIcons = {
   factory,
   house,
   shop,
+  school,
+  publicTransport,
+  usine,
+  church,
+  mall,
+  airport,
+  cityHall,
 };
